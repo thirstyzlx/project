5502d0e08200c9ed9b7af36b5710230ef94fc905
Luxi <l4zou@uwaterloo.ca>
2015-08-27 05:38:20 -0400

    Update info mock

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Users::SessionsController < Devise::SessionsController
+# before_filter :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message :notice, :"Sign in failed: #{:signed_in}"
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    #respond_with resource, location: root_path
+    redirect_to request.referer
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.for(:sign_in) << :attribute
+  # end
+end
